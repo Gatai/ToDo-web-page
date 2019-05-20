2c5cf0abd94cc32d8ddc8bbd849f3de75606e192
Novak Bubanja <bubanjanovak@gmail.com>
2019-05-20 15:11:23 +0200

    novak push2

--- events.js
+++ events.js
@@ -2,11 +2,22 @@ const form1 = document.getElementById("form1");
 
 const rows =document.getElementById("rows");
 
+let itemsleft = document.querySelector("#itemsleft");
+
+
+let counter = -1;
+let checkcounter = 0;
+let numremoved = 0;
+
 form1.addEventListener("keyup", function (e) {
 
     if (e.keyCode == 13) {
 
+        counter++;
+
         const task = document.createElement("li");
+        
+        task.id = counter;
 
         const check = document.createElement("input");
 
@@ -14,11 +25,14 @@ form1.addEventListener("keyup", function (e) {
 
         const del  = document.createElement("button");
 
+        del.id = counter;
+        del.classList.add("delbutton");
+
        // const del  = document.createElement("img");
         text.textContent = form1.value;
         check.type = "checkbox";
 
-        text.for = "task";
+       // text.for = "task";
 
         rows.appendChild(task);
 
@@ -30,6 +44,34 @@ form1.addEventListener("keyup", function (e) {
 
         del.textContent ="X";
 
+        
+
+
+        del.addEventListener("click", event =>
+        {
+              task.remove();
+              numremoved++;
+        });
+
+        
+        check.addEventListener("click", event =>
+        {
+            
+            checkcounter++;
+            let sum = document.createElement("span");
+            sum.textContent = " " + ((counter + 1)-numremoved-checkcounter);
+            itemsleft.appendChild(sum);
+
+        })
     }
 
 });
+
+
+/*
+var thebuttons = document.querySelectorAll('.delbutton');
+
+thebuttons.forEach((b)=>{
+  b.addEventListener('click',(e)=>{prompt(this.id)},false);
+});
+*/
